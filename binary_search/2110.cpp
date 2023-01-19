9b558e96222bd4556f5e320e81123d16f6d40231
blanket77 <quantummik@naver.com>
2023-01-19 23:40:43 +0900

    Je19

--- binary_search/2110.cpp
+++ binary_search/2110.cpp
@@ -4,41 +4,43 @@
 
 using namespace std;
 
-long long arr[MAX];
+int arr[MAX];
 int anw = -1;
+int wifi = 0;
 int N, C;
 
 void sol(){
-    int start = arr[0]; int end = arr[N-1];
+    int mid;
 
-    while (start <= end){
-        int mid = (start + end) / 2;
-        int befor_index = 0;
-        int wifi = 1;
+    //간격을 이분탐색할 것이다.
+    int start = 0;
+    int end = arr[N - 1] - arr[0];
 
+    while(start <= end){
+        mid = (start + end)/2; 
+
+        //첫번째 공유기 설치
+        int s = 0; // 첫번째 공유기 위치 인덱스
+        wifi = 1;
         for(int i = 1 ; i < N ; i++){
-            if(arr[i] - arr[befor_index] >= mid){
-                befor_index = i;
+            if( (arr[i] - arr[s]) >=  mid){
                 wifi++;
-            }
+                s = i; //기준 공유기 위치 인덱스 바뀜
+            }             
         }
 
-        if (wifi >= C){
-            start = mid +1;
-            anw = max(anw, mid);
+        if(wifi >= C){
+            anw = mid;
+            start = mid + 1;
+            
         }
+
         else{
-            end = mid -1;
+            end = mid - 1 ;
         }
     }
 
- 
-    // arr[mid] 만큼 건너 띤다. wift++ 하고 
-    // 만약 건너뛰었을 때 최대 거리를 뛰어넘었을 때 C와 비교한다.
-    // 찾은 개수가 C와 같으면 거리가 더 멀 수 있으니 start = mid +1로 한다.
-    // 찾은 개수가 C보다 크면 거리를 늘려야해서 start = mid +1이어야한다.
-    // 찾은 개수가 C보다 작으면 거리를 줄여야해서 end = mid - 1 이어야한다.
-    
+
 }
 
 int main(){
@@ -48,14 +50,14 @@ int main(){
 
     cin >> N >> C;
 
-    for(int i = 0 ; i < N; i++){
-        cin >> arr[i]; 
+    for(int i = 0 ; i < N ; i++){
+        cin >> arr[i];
     }
 
     sort(arr, arr + N);
-    
-    sol();
 
-    cout << anw << '\n';
+    sol();
+   
+    cout << anw << '\n';    
     return 0;            
 }
